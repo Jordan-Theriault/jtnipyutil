58e30109b7f345faa452bf0022e5d877fc093b74
Jordan <jtheriault7@gmail.com>
2018-09-12 18:41:06 -0400

    added level 1 workflow

--- jt_modeling.py
+++ jt_modeling.py
@@ -210,12 +210,225 @@ def create_lvl2tfce_wf(fwhm_list, full_cons, use_mask=False):
         ])
     return lvl2tfce_wf
 
-def create_lvl1model_wf(subject_list):
+def create_lvl1pipe_wf(subject_list):
     import nipype.pipeline.engine as pe # pypeline engine
+    import nipype.interfaces.fsl as fsl
+    import os
+    from nipype import IdentityInterface, SelectFiles
+    from nipype.interfaces.utility.wrappers import Function
 
+    ##################  Setup workflow.
+    lvl1pipe_wf = pe.Workflow(name='lvl_one_pipe')
 
-    from nipype import IdentityInterface
-    # Select Subject and Run
+    inputspec = pe.Node(IdentityInterface(
+        fields=['input_dir', #TODO - FIX
+                'output_dir', #TODO - FIX
+                'mask_file', #TODO - FIX
+                'subject_list', #TODO - FIX
+                'con_regressors', #TODO - FIX
+                'sinker_subs', #TODO - FIX
+                ],
+        mandatory_inputs=False),
+                 name='inputspec')
+
     infosource = pe.Node(IdentityInterface(fields=['subject_id']),
                      name='infosource')
     infosource.iterables = [('subject_id', subject_list)]
+
+    ################## Select Files
+    data_grab = pe.Node(SelectFiles(templates), name='data_grab')
+    data_grab.inputs.base_directory  = study_dir #TODO - set in workflow
+    # data_grab.inputs.subject_id # From infosource
+
+    ################## Setup confounds
+    def get_terms(confound_file, noise_transforms, noise_regressors, TR, options):
+        # # Add time derivs, quadratic terms, and quad time derives if requested.
+        # TODO - allow transforms to apply selectively.
+        import numpy as np
+        import pandas as pd
+        tf_cf = confound_file
+        df_cf = pd.DataFrame(pd.read_csv(tf_cf, sep='\t', parse_dates=False))
+        confounds = df_cf[noise_regressors] # for output
+        base = df_cf[noise_regressors] # for transforms
+        TR_time = pd.Series(np.arange(0.0, TR*base.shape[0], TR)) # time series for derivatives.
+        if 'quad' in noise_transforms:
+            quad = np.square(df_cf[noise_regressors])
+            confounds = confounds.join(quad, rsuffix='_quad')
+        if 'tderiv' in noise_transforms:
+            tderiv = pd.DataFrame(pd.Series(np.gradient(base[col]), TR_time)
+                                  for col in base).T
+            confounds = confounds.join(quad, rsuffix='_tderiv')
+        if 'quadtderiv' in noise_transforms:
+            quadtderiv = np.square(tderiv)
+            confounds = confounds.join(quad, rsuffix='_quadtderiv')
+        if options['remove_steadystateoutlier']:
+            confounds = confounds.join(df_cf[df_cf.columns[df_cf.columns.to_series().str.contains('^NonSteadyStateOutlier')]])
+        if options['ICA_AROMA']:
+            confounds = confounds.join(df_cf[df_cf.columns[df_cf.columns.to_series().str.contains('^AROMAAggrComp')]])
+        return confounds
+
+    get_confounds = pe.Node(Function(input_names=['confound_file', 'noise_transforms',
+                                                  'noise_regressors', 'TR', 'options'],
+                                 output_names=['confounds'],
+                                  function=get_terms),
+                         name='get_confounds')
+    # get_confounds.inputs.confound_file =  # From data_grab
+    get_confounds.inputs.noise_transforms =  noise_transforms #TODO - set in workflow
+    get_confounds.inputs.noise_regressors =  noise_regressors #TODO - set in workflow
+    get_confounds.inputs.TR = TR #TODO - set in workflow
+    get_confounds.inputs.options = options #TODO - set in workflow
+
+    ################## Create bunch to run FSL first level model.
+    def get_subj_info(task_file, design_col, confounds, params):
+        # Makes a Bunch, giving all necessary data about conditions, onsets, and durations to
+        # FSL first level model. Needs a task file to run.
+        from nipype.interfaces.base import Bunch
+        import pandas as pd
+        import numpy as np
+        output = []
+        tf = task_file
+        df = pd.DataFrame(pd.read_csv(tf, sep='\t', parse_dates=False))
+        output = Bunch(conditions= params,
+                           onsets=[list(df[df[design_col] == f].onset) for f in params],
+                           durations=[list(set(df[df[design_col] == f].duration)) for f in params],
+                           amplitudes=None,
+                           tmod=None,
+                           pmod=None,
+                           regressor_names=confounds.columns.values,
+                           regressors=confounds.T.values.tolist()) # movement regressors added here. List of lists.
+        return output
+
+    make_bunch = pe.Node(Function(input_names=['task_file', 'design_col', 'confounds', 'params'],
+                                 output_names=['subject_info'],
+                                  function=get_subj_info),
+                         name='make_bunch')
+    # make_bunch.inputs.task_file =  # From data_grab
+    # make_bunch.inputs.confounds =  # From get_confounds
+    make_bunch.inputs.design_col = design_col #TODO - set in workflow
+    make_bunch.inputs.params = params #TODO - set in workflow
+
+    ################## Mask functional data.
+    from nipype.interfaces.fsl.maths import ApplyMask
+    maskBold = pe.Node(ApplyMask(),
+                      name='maskBold')
+    # maskBold.inputs.in_file # From data_grab
+    # maskBold.inputs.mask_file # From data_grab
+
+    ################## Despike
+    from nipype.interfaces.afni import Despike
+    despike = pe.Node(Despike(),
+                      name='despike')
+    # despike.inputs.in_file = # From Mask
+
+    ################## Susan smooth
+    from nipype.interfaces.fsl.maths import MathsCommand
+    smoothsource = pe.Node(IdentityInterface(fields=['fwhm']),
+                     name='smoothsource')
+    smoothsource.iterables = [('fwhm', fwhm_list)]
+
+    intensitymask = pe.Node(MathsCommand(),
+                           name='intensitymask')
+    # intensitymask.inputs.in_file = # from maskBold
+    intensitymask.inputs.args = susan_intensity #TODO - set in workflow
+
+    from nipype.workflows.fmri.fsl.preprocess import create_susan_smooth
+    smooth_wf = create_susan_smooth()
+    # smooth_wf.inputs.inputnode.in_files = # from maskBold
+    # smooth_wf.inputs.inputnode.fwhm = # from smoothsource
+
+    ################## Model Generation.
+    import nipype.algorithms.modelgen as model # FSL Specify Model - generate design information
+    specify_model = pe.Node(interface=model.SpecifyModel(), name='specify_model')
+    specify_model.inputs.high_pass_filter_cutoff = hpf_cutoff #TODO - set in workflow
+    specify_model.inputs.input_units = 'secs' #TODO - set in workflow
+    specify_model.inputs.time_repetition = TR #TODO - set in workflow
+    # specify_model.functional_runs # From data_grab
+    # specify_model.subject_info # From subject_info
+
+    ################## Estimate workflow
+    from nipype.workflows.fmri.fsl import estimate # fsl workflow
+    modelfit = estimate.create_modelfit_workflow()
+    modelfit.base_dir = '.'
+    # modelfit.inputs.inputspec.session_info = # From specify_model
+    # modelfit.inputs.inputspec.functional_data = # from maskBold
+    modelfit.inputs.inputspec.interscan_interval = TR #TODO - set in workflow
+    modelfit.inputs.inputspec.film_threshold = FILM_threshold #TODO - set in workflow
+    modelfit.inputs.inputspec.bases = bases #TODO - set in workflow
+    modelfit.inputs.inputspec.model_serial_correlations = model_serial_correlations #TODO - set in workflow
+    modelfit.inputs.inputspec.contrasts = contrasts #TODO - set in workflow
+
+    ################## DataSink
+    from nipype.interfaces.io import DataSink
+    import os.path
+    sinker = pe.Node(DataSink(), name='sinker')
+    sinker.inputs.substitutions = sinker_substitutions #TODO - set in workflow
+    if options['smooth']:
+        sinker.inputs.base_directory = os.path.join(output_dir, 'smooth')
+        if not os.path.isdir(sinker.inputs.base_directory):
+            os.mkdir(sinker.inputs.base_directory)
+    else:
+        sinker.inputs.base_directory = os.path.join(output_dir, 'nosmooth')
+        if not os.path.isdir(sinker.inputs.base_directory):
+            os.mkdir(sinker.inputs.base_directory)
+
+    lvl1pipe_wf.connect([
+        # grab subject/run info
+        (infosource, data_grab, [('subject_id', 'subject_id')]),
+        (data_grab, get_confounds, [('confound_file', 'confound_file')]),
+        (get_confounds, make_bunch, [('confounds', 'confounds')]),
+        (data_grab, make_bunch, [('task', 'task_file')]),
+        (make_bunch, specify_model, [('subject_info', 'subject_info')]),
+        (data_grab, maskBold, [('bold', 'in_file'),
+                                 ('bold_mask', 'mask_file')])])
+
+    if options['censoring'] == 'despike':
+        lvl1pipe_wf.connect([
+            (maskBold, despike, [('out_file', 'in_file')])])
+        if options['smooth']:
+            lvl1pipe_wf.connect([
+                (despike, intensitymask, [('out_file', 'in_file')]),
+                (smoothsource, smooth_wf, [('fwhm', 'inputnode.fwhm')]),
+                (intensitymask, smooth_wf, [('out_file', 'inputnode.mask_file')]),
+                (intensitymask, sinker, [('out_file', 'smoothing')]),
+                (despike, smooth_wf, [('out_file', 'inputnode.in_files')]),
+                (smooth_wf, specify_model, [('outputnode.smoothed_files', 'functional_runs')]),
+                (smooth_wf, modelfit, [('outputnode.smoothed_files', 'inputspec.functional_data')])])
+        else:
+            lvl1pipe_wf.connect([
+                (despike, specify_model, [('out_file', 'functional_runs')])]
+                (despike, modelfit, [('out_file', 'inputspec.functional_data')]))
+            #TODO connect despike to sinker, to check output.
+    else:
+        if options['smooth']:
+            lvl1pipe_wf.connect([
+                (maskBold, intensitymask, [('out_file', 'in_file')]),
+                (smoothsource, smooth_wf, [('fwhm', 'inputnode.fwhm')]),
+                (intensitymask, smooth_wf, [('out_file', 'inputnode.mask_file')]),
+                (intensitymask, sinker, [('out_file', 'smoothing')]),
+                (maskBold, smooth_wf, [('out_file', 'inputnode.in_files')]),
+                (smooth_wf, specify_model, [('outputnode.smoothed_files', 'functional_runs')]),
+                (smooth_wf, modelfit, [('outputnode.smoothed_files', 'inputspec.functional_data')])])
+        else:
+            lvl1pipe_wf.connect([
+                (maskBold, specify_model, [('out_file', 'functional_runs')]),
+                (maskBold, modelfit, [('out_file', 'inputspec.functional_data')])])
+
+    lvl1pipe_wf.connect([
+        (specify_model, modelfit, [('session_info', 'inputspec.session_info')]),
+        (infosource, sinker, [('subject_id','container')]), # creates folder for each subject.
+        (modelfit, sinker, [('outputspec.dof_file','model.@dof'), #.@ puts this in the par folder.
+                            ('outputspec.parameter_estimates', 'model'), #TODO - grab design .mat file.
+                            ('outputspec.copes','model.@copes'),
+                            ('outputspec.varcopes','model.@varcopes'),
+                            ('outputspec.zfiles','stats'),
+                            ('outputspec.pfiles', 'stats.@pfiles'),
+                            ('level1design.ev_files', 'design'),
+                            ('level1design.fsf_files', 'design.@fsf'),
+                            ('modelgen.con_file', 'design.@confile'),
+                            ('modelgen.design_cov', 'design.@covmatriximg'),
+                            ('modelgen.design_image', 'design.@designimg'),
+                            ('modelestimate.logfile', 'design.@log'),
+                            ('modelestimate.residual4d', 'model.@resid'),
+                            ('modelestimate.fstats', 'stats.@fstats'),
+                           ])
+        ])
