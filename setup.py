5a85e4ad6cc1f917e128b0a26304cc398f97aeed
Jordan <jtheriault7@gmail.com>
2018-10-17 18:36:51 -0400

    fix setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = '0.1',
     author = 'Jordan Theriault',
     author_email = 'jordan_theriault@northeastern.edu',
-    packages = find_packages(exclude=['examples', 'jt_7t.py'])
-    license = 'LICENSE'
-    long_description='a personal collection of python tools to simplify analysis using nipype.'
+    packages = find_packages(exclude=['examples', 'jt_7t.py']),
+    license = 'LICENSE',
+    long_description='a personal collection of python tools to simplify analysis using nipype.',
 )
