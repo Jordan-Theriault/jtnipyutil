19f43864d966ae185faa6c898df93b7f6312bf7b
Jordan <jtheriault7@gmail.com>
2018-10-17 18:35:48 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = '',
+    version = '0.1',
+    author = 'Jordan Theriault',
+    author_email = 'jordan_theriault@northeastern.edu',
+    packages = find_packages(exclude=['examples', 'jt_7t.py'])
+    license = 'LICENSE'
+    long_description='a personal collection of python tools to simplify analysis using nipype.'
+)
