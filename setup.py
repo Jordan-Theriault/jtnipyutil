fdccdb13360cfc786603633a19322496cc57a7fc
Jordan <jtheriault7@gmail.com>
2018-10-22 14:02:17 -0400

    fix name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'pyfmriutil',
+    name = 'jtnipyutil',
     version = '0.1',
     author = 'Jordan Theriault',
     author_email = 'jordan_theriault@northeastern.edu',
