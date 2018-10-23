3eff639a062e91e58bfd8bc344d6a1b9283bad88
Jordan <jtheriault7@gmail.com>
2018-10-23 18:55:34 -0400

    added dockerfile info for nipymc

--- neurodocker_command.sh
+++ neurodocker_command.sh
@@ -9,14 +9,18 @@ docker run --rm kaczmarj/neurodocker:master generate docker \
 --freesurfer version=6.0.0-min \
 --afni version=latest \
 --spm version=r7219 \
---miniconda create_env=py36 \
-  conda_install="python=3.6 jupyter jupyterlab jupyter_contrib_nbextensions
-                 traits pandas matplotlib scikit-learn seaborn" \
+--miniconda create_env=py35 \
+  conda_install="python=3.5 jupyter jupyterlab jupyter_contrib_nbextensions
+                 traits pandas matplotlib scikit-learn seaborn mkl nose sphinx
+                 theano pygpu" \
   pip_install="https://github.com/nipy/nipype/tarball/master
-               https://github.com/INCF/pybids/tarball/master
-               nltools nilearn datalad[full] nipy duecredit niwidgets
-               mne deepdish hypertools ipywidgets pynv six nibabel joblib
-               git+https://github.com/poldracklab/fmriprep.git
-               git+https://github.com/poldracklab/niworkflows.git" \
+              https://github.com/INCF/pybids/tarball/master
+              nltools nilearn datalad[full] nipy duecredit niwidgets
+              mne deepdish hypertools ipywidgets pynv six nibabel joblib
+              parameterized
+              git+https://github.com/poldracklab/fmriprep.git
+              git+https://github.com/poldracklab/niworkflows.git
+              git+https://github.com/pymc-devs/pymc3
+              git+https://github.com/PsychoinformaticsLab/nipymc" \
   activate=True \
-  --copy jtnipyutil /opt/miniconda-latest/envs/py36/lib/python3.6/site-packages/jtnipyutil
+--copy jtnipyutil /opt/miniconda-latest/envs/py36/lib/python3.6/site-packages/jtnipyutil
