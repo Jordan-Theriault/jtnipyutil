eac0ff9d2ced7082de37f6f777f8d68691a1384e
Jordan <jtheriault7@gmail.com>
2019-11-15 19:27:50 -0500

    updated neurodocker command and Dockerfile to deal with compatability issues

---
+++ neurodocker_command_nov2019.sh
@@ -0,0 +1,23 @@
+docker run --rm kaczmarj/neurodocker:master generate docker \
+--base debian:stretch --pkg-manager apt \
+--install gcc g++ graphviz tree \
+          git vim emacs-nox nano less ncdu \
+          tig openjdk-8-jdk \
+--run "export JCC_JDK=/usr/lib/jvm/java-8-openjdk-amd64" \
+--fsl version=5.0.11 \
+--ants version=2.2.0 \
+--convert3d version=1.0.0 \
+--freesurfer version=6.0.0-min \
+--afni version=latest \
+--spm version=r7219 \
+--miniconda create_env=py36 \
+  conda_install="python=3.6 jupyter jupyterlab jupyter_contrib_nbextensions
+                 traits pandas matplotlib scikit-learn==0.20.3 seaborn" \
+  pip_install="https://github.com/nipy/nipype/tarball/master
+               https://github.com/INCF/pybids/tarball/0.9.4
+               nltools nilearn datalad[full] nipy duecredit niwidgets
+               mne deepdish hypertools ipywidgets pynv six nibabel joblib==0.11
+               git+https://github.com/poldracklab/fmriprep.git
+               git+https://github.com/poldracklab/niworkflows.git" \
+  activate=True \
+--copy jtnipyutil /opt/miniconda-latest/envs/py36/lib/python3.6/site-packages/jtnipyutil
