41236437f7a85b4d8de29c9d14e37866f2fe3047
Jordan <jtheriault7@gmail.com>
2019-11-20 17:30:44 -0500

    added streamlined laminar mask example from roi

--- examples/laminar/laminar_mask_roi/laminar_mask_roi.py
+++ examples/laminar/laminar_mask_roi/laminar_mask_roi.py
@@ -1,65 +1,40 @@
-import os, glob
-import numpy as np
-import pandas as pd
-import nibabel as nib
-import matplotlib as mpl
-mpl.use('Agg')
-import matplotlib.pyplot as plt
-from nilearn.image import resample_img
-from nilearn.input_data import NiftiMasker
+def laminar_mask_roi_list(subj, depth_file, img_file, conf_file, work_dir, roi_dir, out_label,
+                          conf_names, detrend_tf):
+    import os, glob
+    import numpy as np
+    import pandas as pd
+    import nibabel as nib
+    import matplotlib as mpl
+    from scipy.stats.stats import pearsonr
+    # mpl.use('Agg')
+    # import matplotlib.pyplot as plt
+    from nilearn.image import resample_img, math_img
+    from nilearn.input_data import NiftiMasker
+    roi_files = glob.glob(os.path.join(roi_dir, '*.nii'))
+    ######## Fit atlas to the resting state data (affine and shape)
+    roi_refit_files = []
+    for roi in roi_files:
+        fit_atlas = resample_img(nib.load(roi),
+                                         target_affine=nib.load(img_file).affine,
+                                         target_shape=nib.load(img_file).shape[0:3],
+                        interpolation='nearest')
+        fit_atlas_data = np.round(fit_atlas.get_data(), 0)
+        nib.save(nib.Nifti1Image(fit_atlas_data, fit_atlas.affine,
+                             fit_atlas.header), os.path.join(work_dir, 'ALIGN_'+roi.split('/')[-1]))
+        roi_refit_files.append(os.path.join(work_dir, 'ALIGN_'+roi.split('/')[-1]))
 
-root = '/scratch/'+os.environ['USER']+'/'+os.environ['SUBJ']+'/'+os.environ['PROJNAME']
-sanity_check = True
-subj = os.environ['SUBJ']
-depth_file = root+'/data/'+os.environ['DEPTH']
-img_file = root+'/data/'+os.environ['IMG']
-conf_file = root+'/data/'+os.environ['CONFOUND']
-
-work_dir = root+'/nighres'
-mask_list = ['dACC_Wager.rh.sm08.vol.nii.gz',
-             'dAmy_Gianaros.rh.sm08.vol.nii.gz',
-             'dmIns_Kurth.rh.sm08.vol.nii.gz',
-             'dpIns_Gianaros.rh.sm08.vol.nii.gz',
-             'lvAIns_Wager.rh.sm08.vol.nii.gz',
-             'mvAIns_Harper.rh.sm08.vol.nii.gz',
-             'pgACC_Gianaros.rh.sm08.vol.nii.gz',
-             'sgACC_Gianaros.rh.sm08.vol.nii.gz'] # TODO this mask list will have to come from the atlas instead.
-conf_names = ['^AROMAAggrComp', 'WhiteMatter', 'CSF'] # see Ciric et al., 2017, Neuroimage SET VARIABLE TO INCLUDE CONFOUNDS. ACCEPTS A STRING, OR A LIST OF STRINGS.
-# conf_names = 'TCompCor' # see following ANTS normalization, used in Kashyap et al., 2018, Sci Reports
-detrend_tf = False
-
-######## Fit cortical depth to the resting state data (affine and shape), then flatten.
-fit_depth = resample_img(nib.load(depth_file),
-                                 target_affine=nib.load(img_file).affine,
-                                 target_shape=nib.load(img_file).shape[0:3])
-fit_depth_data = np.round(fit_depth.get_data(), 2)
-# rounding to 2 here kept the distribution similar to when the cortical depth was not transformed to
-# bold space.
-nib.save(nib.Nifti1Image(fit_depth_data, fit_depth.affine,
-                         fit_depth.header), os.path.join(work_dir, 'ALIGN_'+depth_file.split('/')[-1]))
-depth_flat = np.reshape(fit_depth_data, np.prod(fit_depth_data.shape))
-
-fig, ax = plt.subplots(nrows=len(mask_list), ncols=2)
-
-for idx, mask in enumerate(mask_list):
-    print('getting data for mask: %s' % mask.split('.')[0])
-    # Fit mask to the resting state data (affine and shape).
-    mask_file = root+'/data/'+mask
-    fit_mask = resample_img(nib.load(mask_file),
+    ####### Fit cortical depth to the resting state data (affine and shape)
+    fit_depth = resample_img(nib.load(depth_file),
                                      target_affine=nib.load(img_file).affine,
-                                     target_shape=nib.load(img_file).shape[0:3],
-                    interpolation='nearest')
-    fit_mask_data = np.round(fit_mask.get_data(), 0)
-    nib.save(nib.Nifti1Image(fit_mask_data, fit_mask.affine,
-                         fit_mask.header), os.path.join(work_dir, 'ALIGN_'+mask_file.split('/')[-1]))
+                                     target_shape=nib.load(img_file).shape[0:3]) # this should be fine.
+    fit_depth_data = np.round(fit_depth.get_data(), 2) # rounding to 2 here kept the distribution similar to when the cortical depth was not transformed to bold space.
+    nib.save(nib.Nifti1Image(fit_depth_data, fit_depth.affine,
+                             fit_depth.header), os.path.join(work_dir, 'ALIGN_'+depth_file.split('/')[-1]))
+    depth_refit = nib.load(os.path.join(work_dir, 'ALIGN_'+depth_file.split('/')[-1]))
 
-    # Mask flattened depth
-    mask_flat = np.reshape(fit_mask_data, np.prod(fit_mask_data.shape))
-    depth_masked = depth_flat[mask_flat>0]
-
-    # Mask BOLD data, modeling motion confounds.
+    ######## Model whole-brain BOLD data using motion confounds.
     niftimask = NiftiMasker(dtype='float32', t_r=2.34, detrend=detrend_tf)
-    niftimask.fit(fit_mask)
+    niftimask.fit(math_img('img > 0', img=depth_refit))
     ######## Get Confounds, if specified.
     if conf_names:
         conf_raw = pd.read_csv(conf_file, sep='\t')
@@ -67,42 +42,127 @@ for idx, mask in enumerate(mask_list):
             confound_list = conf_raw[conf_raw.columns[conf_raw.columns.to_series().str.contains('|'.join(conf_names))]]
         else:
             confound_list = conf_raw[conf_raw.columns[conf_raw.columns.to_series().str.contains(conf_names)]]
-    img_masked = niftimask.transform(img_file, confounds=confound_list.values)
-    assert img_masked.shape[1] == len(depth_masked), 'masked image and masked depth are not equal length'
+        img_masked = niftimask.transform(img_file, confounds=confound_list.values)
+    else:
+        img_masked = niftimask.transform(img_file)
 
-    # remove any additional voxels with no cortical depth
-    img_masked = img_masked[:, depth_masked != 0]
-    depth_masked = depth_masked[depth_masked != 0]
 
-    #  Get cortical depth in Native space for histogram.
-    depthfit_mask = resample_img(nib.load(mask_file),
-                                 target_affine=nib.load(depth_file).affine,
-                                 target_shape=nib.load(depth_file).shape[0:3])
-    depthfit_mask_data = np.round(depthfit_mask.get_data(), 0)
-    depth_native = nib.load(depth_file).get_data()[depthfit_mask_data>0]
-    depth_native = depth_native[depth_native>0]
+    ######## Mask cortical depth by cortical depth.
+    niftimask_depth = NiftiMasker(dtype='float32')
+    niftimask_depth.fit(math_img('img > 0', img=depth_refit))
+    depth_masked = niftimask_depth.transform(depth_refit)
 
-    # plot and save histogram of cortical depth (TRANSFORMED & NATIVE SPACE)
-    ax[idx, 0].hist(depth_masked, bins=[0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1])
-    ax[idx, 0].set_title('ROI = '+ mask.split('.')[0]+', Transformed')
-#     plt.ylabel('COUNT')
-#     plt.xlabel('CORTICAL DEPTH')
-    ax[idx, 1].set_title('Native')
-    ax[idx, 1].hist(depth_native, bins=[0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1])
+    # atlas may be 2d here, with 4d now on 2nd dimension
+    ######## Mask atlas by cortical depth.
+    corr_list = []
+    roi_output_10 = []
+    roi_countout_10 = []
+    roi_output_3 = []
+    roi_countout_3 = []
+    for idx, roi in enumerate(roi_refit_files):
+        atlas_refit = nib.load(roi)
+        niftimask_atlas = NiftiMasker(dtype='float32')
+        niftimask_atlas.fit(math_img('img > 0', img=depth_refit))
+        atlas_masked = niftimask_atlas.transform(atlas_refit)
+        assert img_masked.shape[-1] == atlas_masked.shape[-1], 'flattened image and flattened atlas are not equal on dimension -1'
 
-    # Sanity check, to make sure mask fits properly
-    if sanity_check:
-        fit_depth_data_temp = np.empty_like(fit_depth_data)
-        fit_depth_data_temp[fit_mask_data==1] = fit_depth_data[fit_mask_data==1]
-        fit_depth_data_temp[fit_mask_data!=1] = 0
-        nib.save(nib.Nifti1Image(fit_depth_data_temp, fit_depth.affine,
-                             fit_depth.header), os.path.join(work_dir, 'sanitycheck_'+mask.split('.')[0]+'_'+depth_file.split('/')[-1]))
+        ######## Correlate cortical depth with BOLD
+        roi_data = img_masked[:, [atlas_masked[:,] == 1][0][0][:]]
+        roi_depth = depth_masked[0][[atlas_masked == 1][0][0][:]]
+        roi_vox_avg = np.average(roi_data, axis=0)
+        try: corr_list = np.vstack((corr_list,
+                            [pearsonr(roi_vox_avg, roi_depth)[0], # Correlation between BOLD and cortical depth.
+                             pearsonr(roi_vox_avg, roi_depth)[1], # p value
+                             roi_data.shape[-1]]))
+        except:
+            corr_list = [pearsonr(roi_vox_avg, roi_depth)[0], # Correlation between BOLD and cortical depth.
+                             pearsonr(roi_vox_avg, roi_depth)[1], # p value
+                             roi_data.shape[-1]]
+        ######## Calculate depth (10 categories)
+        bins = np.linspace(0,1,11)
+        roi_depth_10 = np.digitize(roi_depth, bins, right=True)
+        roi_data_10 = [roi_data[:, roi_depth_10 == i].mean(axis=1) for i in range(1, len(bins))] # average across 10 bins
+        roi_count_10 = [(roi_depth_10 == i).sum() for i in range(1, len(bins))] # N of voxels in each bin
+        try:
+            print('loading data from roi %s into 10-category depth output' % roi.split('/')[-1])
+            roi_output_10 = np.vstack((roi_output_10, roi_data_10))
+            roi_countout_10 = np.hstack((roi_countout_10, roi_count_10))
+        except:
+            print('starting 10 depth category output')
+            roi_output_10 = roi_data_10
+            roi_countout_10 = roi_count_10
+        ######## Calculate depth (3 categories)
+        bins = np.linspace(0,1,4)
+        roi_depth_3 = np.digitize(roi_depth, bins, right=True)
+        roi_data_3 = [roi_data[:, roi_depth_3 == i].mean(axis=1) for i in range(1, len(bins))] # average across 3 bins
+        roi_count_3 = [(roi_depth_3 == i).sum() for i in range(1, len(bins))] # N of voxels in each bin
+        try:
+            print('loading data from roi %s into 3-category depth output' % roi.split('/')[-1])
+            roi_output_3 = np.vstack((roi_output_3, roi_data_3))
+            roi_countout_3 = np.hstack((roi_countout_3, roi_count_3))
+        except:
+            print('starting 3 depth category output')
+            roi_output_3 = roi_data_3
+            roi_countout_3 = roi_count_3
 
-    # Then save the TRs as an array, with the depth as the first row.
-    depth_df = pd.DataFrame(depth_masked).transpose()
-    out_df = depth_df.append(pd.DataFrame(img_masked), ignore_index=True)
-    out_df.to_csv(os.path.join(work_dir, 'depth_and_data_'+mask.split('.')[0]+'_'+img_file.split('/')[-1].split('.nii.gz')[0]+'.csv'), # TODO rather than saving this, we'll need to calculate the correlation (signal ~ depth) and save it for the subject and ROI. Output should be a long format table of ROI, subjects, and correlation values. Then, calculate the average correlation (and SE) for each ROI, and plot on an atlas.
-                 index=False, header=False)
+    ######## Save correlations
+    # corr_out = pd.DataFrame({'subj':np.repeat(subj, len(roi_refit_files)),
+    #                          'roi':roi_refit_files,
+    #                          'BOLD_depth_corr':corr_list[:,0],
+    #                         'p_val':corr_list[:,1],
+    #                         'ROI_n':corr_list[:,2]})
+    # corr_out.to_csv(os.path.join(work_dir, 'correlation_'+atlas.split('.')[0]+'_'+img_file.split('/')[-1].split('.nii.gz')[0]+'.csv'),
+    #              index=False, header=True)
 
-plt.tight_layout()
-plt.savefig(work_dir + '/corticaldepth_histogram_'+subj+'.png')
+    # ######## Save depth 10
+    depth10_out = pd.DataFrame({'subj':np.repeat(subj, len(np.repeat(roi_refit_files, 10))),
+                                'roi':np.repeat([i.split('/')[-1] for i in roi_refit_files], 10),
+                                'depth_cat':list(range(0,10))*len(roi_refit_files),
+                                'cat_N':roi_countout_10})
+    depth10_temp = pd.DataFrame(roi_output_10)
+    depth10_out = depth10_out.join(depth10_temp)
+    depth10_out.to_csv(os.path.join(work_dir, 'depth10_'+out_label+'_'+img_file.split('/')[-1].split('.nii.gz')[0]+'.csv'),
+                 index=False, header=True)
+
+    ######## Save depth 3
+    depth3_out = pd.DataFrame({'subj':np.repeat(subj, len(np.repeat(roi_refit_files, 3))),
+                                'roi':np.repeat([i.split('/')[-1] for i in roi_refit_files], 3),
+                                'depth_cat':list(range(0,3))*len(roi_refit_files),
+                                'cat_N':roi_countout_3})
+    depth3_temp = pd.DataFrame(roi_output_3)
+    depth3_out = depth3_out.join(depth3_temp)
+    depth3_out.to_csv(os.path.join(work_dir, 'depth3_'+out_label+'_'+img_file.split('/')[-1].split('.nii.gz')[0]+'.csv'),
+                 index=False, header=True)
+
+    ########  Replace values in atlas and save nifti.
+    # out_atlas = np.zeros(fit_atlas_data.shape)
+    # for idx, corr in enumerate(corr_list):
+    #     out_atlas[fit_atlas_data==np.unique(atlas_masked)[1:][idx]] = corr[0]
+
+    # atlas_head = fit_atlas.header
+    # atlas_head['cal_max'] = 1
+    # atlas_head['cal_min'] = 0
+    # nib.save(nib.Nifti1Image(out_atlas, fit_atlas.affine,
+    #                      atlas_head), os.path.join(work_dir, 'CORR_PSC_w_DEPTH_'+subj+'_'+atlas_file.split('/')[-1]))
+
+    print('done with %s' % subj)
+
+import os
+root = '/scratch/'+os.environ['USER']+'/'+os.environ['SUBJ']+'/'+os.environ['PROJNAME']
+sanity_check = True
+subj = os.environ['SUBJ']
+depth_file = root+'/data/'+os.environ['DEPTH']
+img_file = root+'/data/'+os.environ['IMG']
+conf_file = root+'/data/'+os.environ['CONFOUND']
+work_dir = root+'/nighres'
+roi_dir = root+'/data/rois' # The major difference from the atlas method is here: Specify directory that has .nii files.
+out_label = 'AROMA_WM_CSF_detrend'
+conf_names = ['^AROMAAggrComp', 'WhiteMatter', 'CSF'] # see Ciric et al., 2017, Neuroimage SET VARIABLE TO INCLUDE CONFOUNDS. ACCEPTS A STRING, OR A LIST OF STRINGS.
+detrend_tf = True
+laminar_mask_roi_list(subj, depth_file, img_file, conf_file, work_dir, roi_dir, out_label,
+                          conf_names, detrend_tf)
+out_label = 'NoMotion_NoTrend'
+conf_names = None
+detrend_tf = False
+laminar_mask_roi_list(subj, depth_file, img_file, conf_file, work_dir, roi_dir, out_label,
+                          conf_names, detrend_tf)
