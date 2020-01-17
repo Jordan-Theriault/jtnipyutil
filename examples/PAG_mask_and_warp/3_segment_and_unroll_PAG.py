ce50cc8b702847bea82eb53f46761153f1fde51b
Jordan <jtheriault7@gmail.com>
2020-01-17 12:28:42 -0500

    cleaned naming of PAG masks, and added segmentation script

---
+++ examples/PAG_mask_and_warp/3_segment_and_unroll_PAG.py
@@ -0,0 +1,20 @@
+'''
+docker run -it --rm \
+-v ~/projects/NCI_U01/FSMAP_data/workdir2:/home/neuro/workdir \
+-p 8888:8888 \
+jtheriaultpsych:jtnipyutil
+
+jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
+'''
+
+
+from jtnipyutil.fsmap import segment_and_unroll_PAG
+import os
+
+root = '/home/neuro/workdir/2019-12-04_PAG_lvl2'
+PAG_file = os.path.join(root, 'template/Template_6.nii')
+con_file = os.path.join(root, 'data/dartel/nosmooth/_contrast_2_speech_prep/randomise_raw_tstat1.nii.gz')
+con_name = 'speech_prep'
+out_dir = os.path.join(root, 'output')
+
+segment_PAG(PAG_file, con_file, con_name, out_dir, thresh = .2)
