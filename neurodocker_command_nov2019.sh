ca445ec1bd7d79d096b3cf9703bb8e1d83eb0bff
Jordan <jtheriault7@gmail.com>
2020-04-28 16:53:06 -0400

    updating to latest nipype

--- neurodocker_command_nov2019.sh
+++ neurodocker_command_nov2019.sh
@@ -7,16 +7,15 @@ docker run --rm kaczmarj/neurodocker:master generate docker \
 --fsl version=5.0.11 \
 --ants version=2.2.0 \
 --convert3d version=1.0.0 \
---freesurfer version=6.0.0-min \
+--freesurfer version=6.0.0 \
 --afni version=latest \
 --spm version=r7219 \
 --miniconda create_env=py36 \
   conda_install="python=3.6 jupyter jupyterlab jupyter_contrib_nbextensions
                  traits pandas matplotlib scikit-learn==0.20.3 seaborn" \
-  pip_install="https://github.com/nipy/nipype/tarball/1.2.3
+  pip_install="https://github.com/nipy/nipype/tarball/1.4.2
                https://github.com/INCF/pybids/tarball/0.9.4
                nltools nilearn datalad[full] nipy duecredit niwidgets
-               mne deepdish hypertools ipywidgets pynv six nibabel joblib==0.11
-               git+https://github.com/poldracklab/niworkflows.git" \
+               mne deepdish hypertools ipywidgets pynv six nibabel joblib==0.11" \
   activate=True \
 --copy jtnipyutil /opt/miniconda-latest/envs/py36/lib/python3.6/site-packages/jtnipyutil
